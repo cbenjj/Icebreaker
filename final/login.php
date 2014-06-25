da72157d1044dfa0c010a4306c607ab94be11068
Gerardo Grinman <ggrinman@gmail.com>
2014-06-25 16:21:04 +0300

    p

--- final/login.php
+++ final/login.php
@@ -19,16 +19,27 @@ if ($user) {
 	<head>
 		<meta http-equiv="Content-Type" content="text/html"; charset="UTF-8">
 		<link rel="stylesheet" type="text/css" href="icebreaker.css" />
+		<style>
+		h3 {
+		padding-bottom:100%;
+		}
+	   </style>
 	</head>
 	<header>
 	
 	</header>
 	<body>
 			<div class="logo">
-				<img src="logoicebreaker.png"/>
+				<img src="img/logoicebreaker.png"/>
 			</div>
+			
+			<p class="font2"> The easiest way to meet people without leaving your comfort zone.<br>
+			Make new friends in your city, country or wherever you are. </p>
+			
 			<div class="facebook-btn">
 			    <a href="<?=ROOT?>fb-login.php?deviceid=<?=$_REQUEST['deviceid']?>"><img src="loginfb.png"/></a>
 			</div>
+			
+			<h3> Opportunity awaits! </h3>
 	</body>
 </html>
